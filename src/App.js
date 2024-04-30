91e901d650b38ad74a8e48861e79cb3181116326
varun <varun4@cs.washington.edu>
2024-04-30 13:43:19 -0700

    basic home page and file upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Tutorial from './pages/TutorialPage';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleFileUpload = () => {
+        if (file) {
+            // Placeholder for upload logic
+            console.log('File ready for upload:', file);
+            // Handle the file upload here
+        } else {
+            console.log('No file selected.');
+        }
+    };
+
+    return (
+    <Router>
+        <div className="App">
+            <header className="App-header">
+                <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Protocol Logo" className="App-logo"/>
+            </header>
+            <main>
+                <div className="upload-container">
+                    {/* Updated JSX for the file input and custom buttons */}
+                    <label className="file-input-container">
+                        Choose File
+                        <input type="file" onChange={handleFileChange} accept=".csv" style={{display: 'none'}}/>
+                    </label>
+                    <br/>
+                    <br/>
+                    <button onClick={() => document.querySelector('.file-input-container > input').click()}>
+                        Start Questionnaire
+                    </button>
+                </div>
+            </main>
+            <footer>
+                <Link to="/tutorial">Tutorial</Link>
+            </footer>
+            <Routes>
+                {/*<Route path="/" element={<YourMainComponent/>}/>*/}
+                <Route exact path="/tutorial" element={<Tutorial/>}/>
+            </Routes>
+        </div>
+    </Router>
+    );
+};
 
 export default App;
