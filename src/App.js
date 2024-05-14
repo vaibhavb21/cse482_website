4e6b8049a7eda4c6792aa8ca3a528819030c95dc
Priyanshu Sugasani <sugasanipriyanshu@gmail.com>
2024-05-13 18:26:30 -0700

    Co-authored-by: kingec123 <kingec123@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,106 +1,9 @@
-// import React, { useState, useEffect } from 'react';
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import './App.css';
-// import Tutorial from './pages/TutorialPage';
-// import axios from "axios";
-
-// const MyComponent = () => {
-//     const [tree, setTree] = useState(null);
-
-//     useEffect(() => {
-//         const fetchTree = async () => {
-//             try {
-//                 const response = await axios.get('/get_tree');
-//                 setTree(response.data);
-//             } catch (error) {
-//                 console.error('Error fetching tree:', error);
-//             }
-//         };
-
-//         fetchTree();
-//     }, []);
-
-//     return (
-//         <div>
-//             {/* Render your tree data or handle it however necessary */}
-//             <h1>Tree Data: {JSON.stringify(tree)}</h1>
-//         </div>
-//     );
-// };
-
-// const App = () => {
-//     const [file, setFile] = useState(null);
-
-//     const handleFileChange = (e) => {
-//         setFile(e.target.files[0]);
-//     };
-
-//     const handleFileUpload = () => {
-//         if (file) {
-//             // Placeholder for upload logic
-//             console.log('File ready for upload:', file);
-//             // Handle the file upload here
-//         } else {
-//             console.log('No file selected.');
-//         }
-//     };
-    
-//     const [message, setMessage] = useState('');
-
-//     useEffect(() => {
-//         axios.get('http://localhost:5000/hello')
-//             .then(response => {
-//                 setMessage(response.data.message);
-//             })
-//             .catch(error => {
-//                 // Handle the error here. For example, log the error or set an error message state.
-//                 console.error('Error fetching data:', error);
-//                 setMessage('Failed to load message');
-//             });
-//     }, []);
-
-//     return (
-//     <Router>
-//         <div className="App">
-//             <header className="App-header">
-//                 <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="Protocol Logo" className="App-logo"/>
-//             </header>
-//             <main>
-//                 <div className="upload-container">
-//                     {/* Updated JSX for the file input and custom buttons */}
-//                     <label className="file-input-container">
-//                         Choose File
-//                         <input type="file" onChange={handleFileChange} accept=".csv" style={{display: 'none'}}/>
-//                     </label>
-//                     <br/>
-//                     <br/>
-//                     <button onClick={() => document.querySelector('.file-input-container > input').click()}>
-//                         Start Questionnaire
-//                     </button>
-//                     <MyComponent tree={tree} />
-//                 </div>
-//             </main>
-//             <footer>
-//                 <Link to="/tutorial">Tutorial</Link>
-//             </footer>
-//             <Routes>
-//                 {/*<Route path="/" element={<YourMainComponent/>}/>*/}
-//                 <Route exact path="/tutorial" element={<Tutorial/>}/>
-//             </Routes>
-//         </div>
-//         <div>
-//             <h1>{message}</h1>
-//         </div>
-//     </Router>
-//     );
-// };
-
-// export default App;
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link, Routes, useNavigate} from "react-router-dom";
 import './App.css';
 import Tutorial from './pages/TutorialPage';
 import axios from "axios";
+import StartPage from "./pages/StartPage";
 
 const MyComponent = ({ tree }) => {
     return (
@@ -110,14 +13,22 @@ const MyComponent = ({ tree }) => {
     );
 };
 
+
+
 const App = () => {
     const [file, setFile] = useState(null);
     const [tree, setTree] = useState('');
     const [message, setMessage] = useState('');
+    const [page, setPage] = useState(null);
 
     const handleFileChange = (e) => {
         setFile(e.target.files[0]);
     };
+
+    const handleStartQuestionnaire = (e) => {
+        setPage("./pages/StartPage")
+    }
+
 
     useEffect(() => {
         axios.get('http://localhost:5000/hello')
@@ -134,25 +45,15 @@ const App = () => {
             console.log("response message:")
             console.log(response);
             setTree(response.data);
+ 
+            //setCurrentNode(response.data); 
         })
         .catch(error => {
             console.error('Error fetching data:', error);
             setTree('Failed to load message');
         });
-
-        // const fetchTree = async () => {
-        //     try {
-        //         const response = await axios.get('/get_tree');
-        //         setTree(response.data);
-        //     } catch (error) {
-        //         console.error('Error fetching tree:', error);
-        //     }
-        // };
-
-        // fetchTree();
     }, []);
-
-
+      
     return (
         <Router>
             <div className="App">
@@ -165,25 +66,25 @@ const App = () => {
                             Choose File
                             <input type="file" onChange={handleFileChange} accept=".csv" style={{ display: 'none' }} />
                         </label>
-                        <button onClick={() => document.querySelector('.file-input-container > input').click()}>
-                            Start Questionnaire
-                        </button>
-                        <MyComponent tree={tree} />
+                        {<button onClick={handleStartQuestionnaire}>
+                            <Link to="/StartPage">Start Page</Link>
+                        </button> }
+                        {/* <MyComponent tree={tree} /> */}
                     </div>
                 </main>
-                <footer>
-                    <Link to="/tutorial">Tutorial</Link>
-                </footer>
-                <Routes>
-                    <Route path="/tutorial" element={<Tutorial />} />
-                </Routes>
+                    
+                
+                {/* <AppRoutes /> */}
             </div>
-            <div>
+            <Routes>
+                        <Route path = '/StartPage' element={<StartPage />} />
+                    </Routes>
+            {/* <div>
                 <h1>{message}</h1>
             </div>
             <div>
                 <h1>{JSON.stringify(tree)}</h1>
-            </div>
+            </div> */}
         </Router>
     );
 };
---
+++ src/pages/StartPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const StartPage = () => {
+    return (
+        <div>
+            <h1>Start Questionnaire</h1>
+            <p>This is the start page for the questionnaire.</p>
+        </div>
+    );
+};
+
+export default StartPage;
